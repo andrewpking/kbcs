52aeb3a87f3239f189a623fcf923670897b30eeb
Drew King <andrew.phillip.king@gmail.com>
2024-10-23 14:24:36 -0700

    Videos are now wrapped in an article tag.

--- format-video.php
+++ format-video.php
@@ -1,3 +1,4 @@
+<article>
 <h2 <?php post_class() ?>>							
 	<a href="<?php the_permalink(); ?>"><?php the_title();?></a>
 </h2>
@@ -7,3 +8,4 @@
 <div class="video-container">
 	<?php the_content(); ?>
 </div>
+</article>
