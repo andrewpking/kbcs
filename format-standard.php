1b46a1b23c8d71d9ebbf6bef1d199279e9de5f71
Drew King <andrew.phillip.king@gmail.com>
2024-10-23 14:44:36 -0700

    Articles now contain a title tag.

--- format-standard.php
+++ format-standard.php
@@ -1,4 +1,4 @@
-<article>
+<article title="<?php the_title(); ?>">
 <h2 <?php post_class() ?>><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 <div class="media">
 	<div class="media-left">
